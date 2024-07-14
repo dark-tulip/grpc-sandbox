893e5f3667c17f551087a2f3848cb11d5adf2e6b
ttansh <ttelbayeva@innoforce.kz>
2024-07-14 17:55:38 +0500

    Обработка рандомной серверной ошибки

--- src/main/java/kz/tansh/services/L18BankService.java
+++ src/main/java/kz/tansh/services/L18BankService.java
@@ -2,6 +2,7 @@ package kz.tansh.services;
 
 import com.google.common.util.concurrent.Uninterruptibles;
 import io.grpc.Status;
+import io.grpc.StatusRuntimeException;
 import io.grpc.stub.StreamObserver;
 import kz.tansh.proto.v18.BankAccount;
 import kz.tansh.proto.v18.BankServiceGrpc;
@@ -39,14 +40,29 @@ public class L18BankService extends BankServiceGrpc.BankServiceImplBase {
 
   @Override
   public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
-    RequestValidator.validateAccount(request.getAccountNumber())
-                    .or(() -> RequestValidator.hasEnoughBalance(request.getAmount(), accountRepository.getAccount(request.getAccountNumber()).getBalance()))
-                    .or(() -> RequestValidator.isAmountDivisibleBy10(request.getAmount()))
-                    .map(Status::asRuntimeException)
-                    .ifPresentOrElse(
-                        responseObserver::onError,
-                        () -> sendMoney(request, responseObserver)
-                    );
+
+    try {
+      if (request.getAccountNumber() == 999) {
+        /*
+         * Рандомная ошибка сервера. Если не обработать глобальным exception кинет неясное сообщение
+         * Operation couldn’t be completed because of an unknown error.
+         * Application error processing RPC
+         */
+        throw new RuntimeException("Some random error");
+      }
+
+      RequestValidator.validateAccount(request.getAccountNumber())
+                      .or(() -> RequestValidator.hasEnoughBalance(request.getAmount(), accountRepository.getAccount(request.getAccountNumber()).getBalance()))
+                      .or(() -> RequestValidator.isAmountDivisibleBy10(request.getAmount()))
+                      .map(Status::asRuntimeException)
+                      .ifPresentOrElse(
+                          responseObserver::onError,
+                          () -> sendMoney(request, responseObserver)
+                      );
+    } catch (Exception e) {
+      // благодаря этому блоку обработается рандомная серверная ошибка
+      responseObserver.onError(Status.INTERNAL.withDescription(e.getMessage()).asRuntimeException());
+    }
   }
 
   private void sendMoney(WithdrawRequest request, StreamObserver<Money> responseObserver) {
